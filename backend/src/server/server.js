ef09c2ef1863461d4ac327fae549d858330933b1
Vishal Sharma <sharmavs9205@gmail.com>
2024-08-20 14:00:00 +0530

    backend setup

---
+++ backend/src/server/server.js
@@ -0,0 +1,38 @@
+// Import necessary modules
+import express, { json } from "express";
+import connectDb from "./config/dbconnection.js";
+import cors from "cors";
+import uploadKeRoutes from './routes/uploadRoutes.js';
+import {FRONTEND_URL} from "./constants.js"
+
+// Connect to MongoDB
+connectDb();
+
+// Initialize Express app
+const app = express();
+const port = process.env.PORT || 3002;
+
+// Set up JSON middleware
+app.use(json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Enable CORS
+app.use(
+  cors({
+    origin: `${FRONTEND_URL}`,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use("/api/image",uploadKeRoutes);
+app.get("/",(req,res)=>{
+  res.send("server is working");
+})
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
