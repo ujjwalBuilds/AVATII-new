13da77b2a97e7432ba9a18dc4b02248144c33b25
vipin126 <chandvipin68@gmail.com>
2024-08-22 22:29:07 +0530

    models and provider

---
+++ avatii_driver_app/lib/helperFunction.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:avatii_driver_app/Url.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Helperfunction {
+  // static Future<String?> getUserId() async {
+  //   final prefs = await SharedPreferences.getInstance();
+  //   String? userId = prefs.getString('userId');
+  //   return userId;
+  // }
+
+ static Future<String?> uploadImage(File? image) async {
+    if (image != Null) {
+      print("image is here");
+    }
+    var uri =
+        Uri.parse(Appurls.uploadimageurl);
+
+    var request = http.MultipartRequest('POST', uri);
+    request.files.add(await http.MultipartFile.fromPath('image', image!.path));
+
+    var response = await request.send();
+    var responseData = await http.Response.fromStream(response);
+
+    if (response.statusCode == 200) {
+      var jsonResponse = json.decode(responseData.body);
+      print("succesfull");
+      print(jsonResponse['imageUrl']);
+      return jsonResponse['imageUrl'];
+    } else {
+      print("Failed${responseData.body}");
+    }
+  }
+
+
+
+
+
+
+}
