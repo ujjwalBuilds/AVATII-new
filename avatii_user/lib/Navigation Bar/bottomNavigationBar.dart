17343405d17be8ae9612c811e8a842730bd3d1a5
vipin126 <chandvipin68@gmail.com>
2024-08-24 00:16:45 +0530

    users changes app

--- avatii_user/lib/Navigation Bar/bottomNavigationBar.dart
+++ avatii_user/lib/Navigation Bar/bottomNavigationBar.dart
@@ -1,7 +1,12 @@
 import 'package:avatii/Navigation%20Bar/bottomNavigationMenu.dart';
 import 'package:avatii/providers.dart';
+import 'package:avatii/screens/After%20Login/Bookings%20Screen/bookingsScreen.dart';
+import 'package:avatii/screens/After%20Login/Home%20Screen/homeScreen.dart';
+import 'package:avatii/screens/After%20Login/Profile%20Screen/profileScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:go_router/go_router.dart';
 
 class CustomNavigationBar extends ConsumerWidget {
@@ -16,13 +21,16 @@ class CustomNavigationBar extends ConsumerWidget {
         ref.read(selectedIndexProvider.notifier).state = index;
         switch (index) {
           case 0:
-            context.go('/home-screen');
+            //context.go('/home-screen');
+           Get.to(()=>HomeScreen());
             break;
           case 1:
-            context.go('/bookings-screen');
+            // context.go('/bookings-screen');
+            Get.to(()=>BookingsScreen());
             break;
           case 2:
-            context.go('/profile-screen');
+           // context.go('/profile-screen');
+             Get.to(()=>ProfileScreen());
             break;
         }
       },
